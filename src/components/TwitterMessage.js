d1ae3fae8749c80e09b8c3e5b08e5257c1dcd94f
Meg Gutshall <meghan.gutshall@gmail.com>
2021-05-20 21:32:43 -0400

    commit tree 71af315b2f659c6612f581aa4a4e54b8cccc5990

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,17 +1,39 @@
 import React from "react";
 
 class TwitterMessage extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {};
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      message: ""
+    };
   }
-
+  
+  handleMessage = e => {
+    this.setState({
+      message: e.target.value
+    })
+  }
+  
   render() {
+    let messageLength = this.state.message.length
+    let remainingChars = this.props.maxChars - messageLength
+
+    // console.log("this.state.message:", this.state.message)
+    // console.log("messageLength:", messageLength)
+    // console.log("remainingChars:", remainingChars)
+
     return (
       <div>
-        <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <strong>Your message: </strong>
+        <input
+          type="text"
+          name="message"
+          id="message"
+          value={this.state.message}
+          onChange={e => this.handleMessage(e)}
+        />
+        <p>You have {remainingChars} remaining characters.</p>
       </div>
     );
   }
